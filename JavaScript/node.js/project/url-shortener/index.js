37d79468364a3e209fe37552781cfaa47eeb4168
leekeunhwan <dlrmsghks7@gmail.com>
2018-05-16 15:53:39 +0900

    예제 데이터 테스트

--- JavaScript/node.js/project/url-shortener/index.js
+++ JavaScript/node.js/project/url-shortener/index.js
@@ -1,13 +1,36 @@
 const express = require('express')
 const morgan = require('morgan')
+const randomstring = require('randomstring');
 const app = express()
+
+const urls = [{
+    slug: randomstring.generate(8),
+    longUrl: 'https://www.naver.com'
+}]
+
+
 
 app.use('/static', express.static('public'))
 app.use(morgan('dev'))
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+        urls
+    })
+})
+
+app.get('/:slug', (req, res) => {
+    const urlItem = urls.find(item => item.slug === req.params.slug)
+    if (urlItem) {
+        // 301 로그를 쓰면 영구히 이동한 것이기에 더 이상 그 사실이 바뀜이 없음을 나타내는 것이기에 요청을 더 이상 보내지 않는다.
+        res.redirect(301, urlItem.longUrl);
+    } else {
+        // 301 영구히 이동했다.
+        // 302 있긴 있는데 다른데 있다.
+        res.status(404);
+        res.send('404 Not Found');
+    }
 })
 
 app.listen(3000, () => {
