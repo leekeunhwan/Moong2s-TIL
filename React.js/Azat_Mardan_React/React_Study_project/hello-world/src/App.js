1d022b0744c7717ace12b47088ab7c7a0107e58c
leekeunhwan <dlrmsghks7@gmail.com>
2018-06-07 16:38:29 +0900

    삭제 기능 완료

--- React.js/Azat_Mardan_React/React_Study_project/hello-world/src/App.js
+++ React.js/Azat_Mardan_React/React_Study_project/hello-world/src/App.js
@@ -78,6 +78,17 @@ class App extends Component {
               >
                 완료
               </button>
+              <button
+                onClick={e => {
+                  this.setState({
+                    // 삭제 기능은 이렇게 구현할 수 있다.
+                    // 같은 아이디 (클릭한 대상)은 제외한 것만 반환하는 식으로
+                    todos: todos.filter(t => todo.id !== t.id)
+                  });
+                }}
+              >
+                삭제
+              </button>
             </li>
           ))}
         </ul>
